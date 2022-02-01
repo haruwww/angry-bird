c183788c90076c4e199aa898d137536ecc196a07
haruwww <94932075+haruwww@users.noreply.github.com>
2022-02-01 18:02:38 -0300

    Add files via upload

---
+++ Log.js
@@ -0,0 +1,6 @@
+class Log extends mae{
+  constructor(x, y, width, height) {
+    super(x, y, width, height)
+    this.image = loadImage("sprites/wood2.png");
+  }
+};
---
+++ Pig.js
@@ -0,0 +1,6 @@
+class Pig extends mae{
+  constructor(x, y, width, height) {
+    super(x, y, width, height)
+    this.image = loadImage("sprites/palmeras.png");
+  }
+};
